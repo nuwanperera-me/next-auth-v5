0231eef1c53a7b2dbf28257531ae7a43cfc43c01
Nuwan Perera <nuwanperera613@gmail.com>
2024-08-29 20:25:14 +0530

    chore: add metadata

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,6 +2,28 @@ import { auth } from "@/auth";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "next-auth/react";
 import { NavBar } from "./_components/navbar";
+import { Metadata } from "next";
+
+const title = "next auth v5 guide";
+const creator = "@nuwanperera-me";
+const description = "Next auth v5 guide...";
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  creator: creator,
+  openGraph: {
+    title: title,
+    description: description,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    creator: creator,
+  },
+  metadataBase: new URL("https://next-auth-v5-mu.vercel.app/"),
+};
 
 export default async function ProtectedLayout({
   children,
